9d82a46fed5212cd1bb186f8d34c3a9b2aa70005
Poonyaritkwangthong <117970215+Poonyaritkwangthong@users.noreply.github.com>
2023-12-27 21:09:59 +0700

    Add files via upload

---
+++ resources/views/report.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>report</h1>
+    <div>
+        <a href="{{url('/bio')}}">home</a>
+        <a href="{{url('/report')}}">report</a>
+        <a href="{{url('/propose')}}">propose</a>
+        <a href="{{url('/vote')}}">vote</a>
+    </div>
+</body>
+</html>
